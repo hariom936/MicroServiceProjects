a8ed3643588cb8270d988a09030a073d753e6e7b
hariom936 <hariomverma968@gmail.com>
2024-12-21 16:23:35 +0530

    auth_Login

--- auth_micro/controller/AuthController.ts
+++ auth_micro/controller/AuthController.ts
@@ -1,7 +1,10 @@
 import bcrypt from 'bcrypt';
 import prisma from '../config/db.config';
-
+import jwt from 'jsonwebtoken';
+// import "dotenv";
 class AuthController {
+    //Register
+
     static async register(req, res) {
         try {
             const { email, password, name } = req.body;
@@ -34,6 +37,40 @@ class AuthController {
             return res.status(500).json({ error: 'Something went wrong, please try again.' });
         }
     }
+
+    //Login
+
+    static async login(req, res) {
+        try {
+            const { email, password } = req.body;
+    
+            const user = await prisma.user.findUnique({
+                where: { email },
+            });
+    
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const payload = {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                };
+    
+                const jwtSecret = process.env.JWT_SECRET;
+                if (!jwtSecret) {
+                    throw new Error("JWT_SECRET is not defined in the environment variables.");
+                }
+    
+                const token = jwt.sign(payload, jwtSecret, { expiresIn: "365d" });
+    
+                return res.json({ message: "Login Successfully...", access_token: `Bearer ${token}` });
+            } else {
+                return res.status(401).json({ message: "Invalid credentials" });
+            }
+        } catch (error) {
+            return res.status(500).json({ error: "Something went wrong, please try again." });
+        }
+    }
+    
 }
 
 export default AuthController;
